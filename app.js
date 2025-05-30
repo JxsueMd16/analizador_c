2b374c55f42c07e7a2d94ae9b65fae4ee07f42bf
OscarSaquil <osaquilb@miumg.edu.gt>
2025-05-30 17:32:20 -0600

    Actualizacion automatas

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 const analyzerRoutes = require('./routes/analyzer');
 app.use('/', analyzerRoutes);
 
